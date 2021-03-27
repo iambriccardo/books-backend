7ba96a3fed7acadd92bff6453f7da36b9af19d77
Riccardo Busetti <riccardob36@gmail.com>
2021-03-27 17:34:28 +0100

    Add book entity

--- src/entities/book.ts
+++ src/entities/book.ts
@@ -1,10 +1,41 @@
 import { Document, Model, model, Schema } from 'mongoose';
+import { JTDSchemaType } from 'ajv/dist/jtd';
 
 export interface Book {
     isbn: string;
     title: string;
     description: string;
+    price: number;
+    condition: 'bad' | 'ok' | 'good' | 'great' | 'na';
+    pictures: string[];
+    seller: string;
+    sellerLocation: string;
+    publicationDate: Date;
+    saleDate?: Date;
+    buyer?: string;
 }
+
+export const BookJTDSchemaType: JTDSchemaType<Book> = {
+    properties: {
+        isbn: { type: 'string' },
+        title: { type: 'string' },
+        description: { type: 'string' },
+        price: { type: 'float32' },
+        condition: { enum: ['bad', 'ok', 'good', 'great', 'na'] },
+        pictures: {
+            elements: {
+                type: 'string',
+            },
+        },
+        seller: { type: 'string' },
+        sellerLocation: { type: 'string' },
+        publicationDate: { type: 'timestamp' },
+    },
+    optionalProperties: {
+        saleDate: { type: 'timestamp' },
+        buyer: { type: 'string' },
+    },
+};
 
 export interface BookDocument extends Book, Document {}
 
@@ -13,6 +44,19 @@ const BookSchema: Schema = new Schema(
         isbn: { type: String, required: true },
         title: { type: String, required: true },
         description: { type: String, required: true },
+        price: { type: Number, required: true },
+        condition: {
+            type: String,
+            enum: ['bad', 'ok', 'good', 'great', 'na'],
+            default: 'na',
+            required: true,
+        },
+        pictures: { type: [String], required: true },
+        seller: { type: String, required: true },
+        sellerLocation: { type: String, required: true },
+        publicationDate: { type: Date, required: true },
+        saleDate: { type: Date, required: false },
+        buyer: { type: String, required: false },
     },
     {
         versionKey: false,
--- tests/src/use-cases/books/sell-book.ts
+++ tests/src/use-cases/books/sell-book.ts
@@ -1,8 +1,75 @@
 import { describe } from 'mocha';
-import { use } from 'chai';
+import { expect, use } from 'chai';
 import { sellBookUseCase } from '../../../../src/use-cases/books/sell-book';
 import chaiAsPromised from 'chai-as-promised';
+import { Book, BookModel } from '../../../../src/entities/book';
+import {
+    deleteCollections,
+    destroyTestMongoose,
+    initTestMongoose,
+} from '../../../helpers/mongoose';
 
 use(chaiAsPromised);
 
-describe('sellBookUseCase', function () {});
+describe('sellBookUseCase', function () {
+    before(initTestMongoose);
+
+    after(destroyTestMongoose(BookModel));
+
+    afterEach(deleteCollections(BookModel));
+
+    it('should insert the book into the database if the book is valid and has optional parameters', async function () {
+        const book: Book = {
+            isbn: '978-1-56619-909-4',
+            title: 'Alice in Wonderland',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipisci elit.',
+            price: 20,
+            condition: 'ok',
+            pictures: ['alice_in_wonderland_cover.png'],
+            seller: 'riccardo',
+            sellerLocation: 'Trento',
+            publicationDate: new Date(),
+            saleDate: new Date(),
+            buyer: 'marco',
+        };
+
+        const useCase = sellBookUseCase(book);
+
+        expect(useCase()).to.not.be.rejected;
+        expect(await BookModel.findOne({ isbn: '978-1-56619-909-4' })).to.exist;
+    });
+
+    it("should insert the book into the database if the book is valid and doesn't have optional parameters", async function () {
+        const book: Book = {
+            isbn: '978-1-56619-909-4',
+            title: 'Alice in Wonderland',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipisci elit.',
+            price: 20,
+            condition: 'ok',
+            pictures: ['alice_in_wonderland_cover.png'],
+            seller: 'riccardo',
+            sellerLocation: 'Trento',
+            publicationDate: new Date(),
+        };
+
+        const useCase = sellBookUseCase(book);
+
+        expect(useCase()).to.not.be.rejected;
+        expect(await BookModel.findOne({ isbn: '978-1-56619-909-4' })).to.exist;
+    });
+
+    it('should throw an error if the book is invalid', async function () {
+        const book: any = {
+            isbn: '978-1-56619-909-4',
+            title: 'Alice in Wonderland',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipisci elit.',
+        };
+
+        const useCase = sellBookUseCase(book);
+
+        expect(useCase()).to.be.rejectedWith(Error);
+    });
+});

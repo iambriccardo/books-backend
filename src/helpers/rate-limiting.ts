032ed40e58655d08722133a340f4f57065848e4e
Riccardo Busetti <riccardob36@gmail.com>
2021-06-09 15:59:14 +0200

    Increase rate limit

--- src/helpers/rate-limiting.ts
+++ src/helpers/rate-limiting.ts
@@ -4,7 +4,7 @@ import { respondWithError, TooManyRequestsError } from '../errors/base';
 
 export const defaultRateLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute window
-    max: 50, // start blocking after 50 requests
+    max: 500, // start blocking after 50 requests
     handler(req: Request, res: Response, next: NextFunction) {
         respondWithError(req, res, new TooManyRequestsError());
     },

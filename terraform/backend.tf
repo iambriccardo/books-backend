57c8ae3d68e2491f239a3f165f509873a348ae58
Luca Taddeo <ltaddeo@unibz.it>
2021-11-09 14:16:43 +0100

    skip ci Comment KMS settings

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
 terraform {
  backend "s3" {
-   bucket         = "books-backend-terraformState-687187154654"
+   bucket         = "books-backend-tfstate-687187154654"
    key            = "state/terraform.tfstate"
    region         = "eu-central-1"
-   encrypt        = true
-   kms_key_id     = "alias/terraform-bucket-key"
+  #  encrypt        = true
+  #  kms_key_id     = "alias/terraform-bucket-key"
    dynamodb_table = "terraform-state"
  }
 }
--- terraform/remote-state/state.tf
+++ terraform/remote-state/state.tf
@@ -1,33 +1,34 @@
-resource "aws_kms_key" "terraform-bucket-key" {
-  # A KMS Key allows to encrypt the S3 Bucket
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 10
-  enable_key_rotation     = true
-}
+# resource "aws_kms_key" "terraform-bucket-key" {
+#   # A KMS Key allows to encrypt the S3 Bucket
+#   description             = "This key is used to encrypt bucket objects"
+#   deletion_window_in_days = 10
+#   enable_key_rotation     = true
+# }
 
-resource "aws_kms_alias" "key-alias" {
-  # The KMS Alias will later be used by Terraform when configuring the Backend 
-  name          = "alias/terraform-bucket-key"
-  target_key_id = aws_kms_key.terraform-bucket-key.key_id
-}
+# resource "aws_kms_alias" "key-alias" {
+#   # The KMS Alias will later be used by Terraform when configuring the Backend 
+#   name          = "alias/terraform-bucket-key"
+#   target_key_id = aws_kms_key.terraform-bucket-key.key_id
+# }
 
 resource "aws_s3_bucket" "terraform-state" {
   # This resource creates an s3 Bucket to store the Terraform State
-  bucket = "books-backend-terraformState-687187154654"
+  bucket = "books-backend-tfstate-687187154654"
   acl    = "private"
+  force_destroy = true  # This line allows terraform to destroy a non-empty bucket
 
   versioning {
     enabled = true
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.terraform-bucket-key.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       kms_master_key_id = aws_kms_key.terraform-bucket-key.arn
+  #       sse_algorithm     = "aws:kms"
+  #     }
+  #   }
+  # }
 }
 
 resource "aws_s3_bucket_public_access_block" "block" {
